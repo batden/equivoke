6b904ca569bb886f03f8bb7ee122c4bbb43e5b57
Philippe Jean Guillaumie <batden@sfr.fr>
2025-05-06 15:27:11 +0200

    Fix typo

--- equivoke.sh
+++ equivoke.sh
@@ -368,7 +368,7 @@ set_p_src() {
     exit 1
   fi
 
-  mkdir -p "$mypath/sources"
+  mkdir -p "$mypath"/sources
   p_srcdir="$mypath/sources"
   echo "$p_srcdir" >"$HOME/.cache/ebuilds/storepath"
   printf "\n$green_bright%s $off%s\n\n" "You have chosen: $p_srcdir"
