08fa6b025589e0c04d85e05cc9d5cf20d5ae851b
Philippe Jean Guillaumie <batden@sfr.fr>
2025-06-06 15:51:24 +0200

    Refactor uninstall_enlighten()

--- evakuate.sh
+++ evakuate.sh
@@ -235,10 +235,9 @@ uninstall_enlighten() {
   sleep 2
 
   cd "$HOME"
-
   for ((i = ${#prog_mbs[@]} - 1; i >= 0; i--)); do
     printf "\n\n$red_bright%s %s\n\n" "Uninstalling %s\n" "${prog_mbs[i]}"
-    cd "$esrcdir/enlighten/$i"
+    cd "$esrcdir/enlighten/${prog_mbs[i]}"
     sudo ninja -C build uninstall
     printf "\n"
   done
