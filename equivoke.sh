2b51256e8cee215f1971e1ce21a8d8eaeee7440e
Philippe Jean Guillaumie <batden@sfr.fr>
2026-08-24 10:09:49 +0200

    Refactor error handling

--- equivoke.sh
+++ equivoke.sh
@@ -645,7 +645,7 @@ wayld_go() {
 
 # --- First, display the selection menu... ---
 lo() {
-  trap '{ err_msg "KEYBOARD INTERRUPT."; exit_code=130; mng_err; }' SIGINT
+  trap 'handle_sig 130' SIGINT
 
   usr_input=0
   printf "\n$bold%s $off%s\n" "Please enter the number of your choice:"
